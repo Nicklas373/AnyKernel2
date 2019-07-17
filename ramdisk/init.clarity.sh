3e47a6a9af6749783b6806cc41ab68d72c1ad455
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-07-17 08:44:47 +0700

    ramdisk: add zram configuration
    Some ROMs doesn't have zram, so try to enable it on kernel way
    
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- ramdisk/init.clarity.sh
+++ ramdisk/init.clarity.sh
@@ -4,10 +4,19 @@
 # Allows us to get init-rc-like style
 write() { echo "$2" > "$1"; }
 
+# ZRAM
+write /sys/block/zram0/comp_algorithm lz4
+write /sys/block/zram0/disksize 1G
+write /sys/block/zram0/max_comp_streams 8
+write /proc/sys/vm/page-cluster 0
+
 # CPU Values
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 652000
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq 2016000
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor "schedutil"
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq 652000
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_max_freq 2016000
+write /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor "schedutil"
 write /sys/devices/system/cpu/cpufreq/schedutil/up_rate_limit_us 500
 write /sys/devices/system/cpu/cpufreq/schedutil/down_rate_limit_us 20000
 write /sys/devices/system/cpu/cpufreq/schedutil/iowait_boost_enable 0
@@ -44,9 +53,6 @@ chmod 0644 /sys/module/msm_thermal/core_control/enabled
 write /sys/module/msm_thermal/core_control/enabled 0
 chmod 0644 /sys/module/msm_thermal/vdd_restriction/enabled
 write /sys/module/msm_thermal/vdd_restriction/enabled 0
-write /sys/module/msm_thermal/parameters/core_limit_temp_degC 70
-write /sys/module/msm_thermal/parameters/temp_threshold 60
-write /sys/module/msm_thermal/parameters/poll_ms 1000
 
 # Battery
 write /sys/kernel/fast_charge/force_fast_charge 0
