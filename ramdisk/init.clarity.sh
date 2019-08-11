185f28bb050773b97e8043fe1672dbf0a850829b
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-08-11 14:16:33 +0700

    ramdisk: reset it all
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- ramdisk/init.clarity.sh
+++ ramdisk/init.clarity.sh
@@ -14,6 +14,9 @@ for group in background foreground rt top-app; do
 
 # CPU Values
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq 2016000
+write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 652800
+write /sys/devices/system/cpu/cpufreq/schedutil/up_rate_limit_us 500
+write /sys/devices/system/cpu/cpufreq/schedutil/down_rate_limit_us 20000
 write /sys/devices/system/cpu/cpufreq/schedutil/iowait_boost_enable 0
 
 # SchedTune
@@ -21,7 +24,7 @@ write /dev/stune/background/schedtune.boost 0
 write /dev/stune/foreground/schedtune.prefer_idle 1
 write /dev/stune/top-app/schedtune.prefer_idle 1
 write /dev/stune/top-app/schedtune.boost 0
-write /dev/stune/top-app/schedtune.sched_boost 0
+write /dev/stune/top-app/schedtune.sched_boost 1
 
 # Power Efficient Workqueue
 chmod 0644 /sys/module/workqueue/parameters/power_efficient
@@ -34,10 +37,10 @@ chmod 0644 /sys/module/msm_thermal/core_control/enabled
 write /sys/module/msm_thermal/core_control/enabled 0
 chmod 0644 /sys/module/msm_thermal/vdd_restriction/enabled
 write /sys/module/msm_thermal/vdd_restriction/enabled 0
-write /sys/module/msm_thermal/parameters/core_limit_temp_degC 60
-write /sys/module/msm_thermal/parameters/limit_temp_degc 55
-write /sys/module/msm_thermal/parameters/temp_threshold 50
-write /sys/module/msm_thermal/parameters/poll_ms 1000
+write /sys/module/msm_thermal/parameters/core_limit_temp_degC 70
+write /sys/module/msm_thermal/parameters/limit_temp_degc 60
+write /sys/module/msm_thermal/parameters/temp_threshold 55
+write /sys/module/msm_thermal/parameters/poll_ms 500
 
 # GPU Values
 write /sys/class/kgsl/kgsl-3d0/devfreq/governor "msm-adreno-tz"
