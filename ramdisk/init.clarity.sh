402c6ee55e1a90b29fdfe9206ea14bd311f60a73
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-08-20 14:17:35 +0700

    Revert "ramdisk: let's test this"
    Not sure for this
    
    This reverts commit 4c5c954bb8d82b7af877cf0e38d18ad8af4d38da.

--- ramdisk/init.clarity.sh
+++ ramdisk/init.clarity.sh
@@ -18,6 +18,7 @@ write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 652800
 write /sys/devices/system/cpu/cpufreq/schedutil/iowait_boost_enable 0
 
 # Power Efficient Workqueue
+chmod 0644 /sys/module/workqueue/parameters/power_efficient
 write /sys/module/workqueue/parameters/power_efficient Y
 
 # GPU Values
