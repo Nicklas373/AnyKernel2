ed98e1907b5082ca5bb0e8d56806a4d9ab2413d6
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-08-22 20:29:11 +0700

    ramdisk: clean up a bit
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- ramdisk/init.clarity.sh
+++ ramdisk/init.clarity.sh
@@ -13,19 +13,11 @@ for group in background foreground rt top-app; do
     done
 
 # CPU Values
-write /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq 2016000
-write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 652800
-write /sys/devices/system/cpu/cpufreq/schedutil/iowait_boost_enable 0
+write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 0
 
 # Power Efficient Workqueue
 chmod 0644 /sys/module/workqueue/parameters/power_efficient
 write /sys/module/workqueue/parameters/power_efficient Y
-
-# GPU Values
-write /sys/class/kgsl/kgsl-3d0/devfreq/governor "msm-adreno-tz"
-write /sys/class/kgsl/kgsl-3d0/max_gpuclk 650000000
-write /sys/class/kgsl/kgsl-3d0/devfreq/max_freq 650000000
-write /sys/class/kgsl/kgsl-3d0/devfreq/min_freq 133330000
 
 # Switch to CFQ I/O scheduler
 write /sys/block/mmcblk0/queue/scheduler cfq
