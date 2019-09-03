87d41f0daf420a0423d0600591cb7f6e33c90bef
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-09-03 18:09:06 +0700

    ramdisk: configure schedutil & remove schedtune init
    * Change schedutil value based on latest Alvian's recommended config
    * Remove schedtune configuration due hardcoded using STUNE_ASSIST v2 on kernel
    
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- ramdisk/init.clarity.sh
+++ ramdisk/init.clarity.sh
@@ -14,16 +14,9 @@ for group in background foreground rt top-app; do
 
 # CPU Values
 write /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq 0
-write /sys/devices/system/cpu/cpufreq/schedutil/up_rate_limit_us 9000
-write /sys/devices/system/cpu/cpufreq/schedutil/down_rate_limit_us 500
+write /sys/devices/system/cpu/cpufreq/schedutil/up_rate_limit_us 4500
+write /sys/devices/system/cpu/cpufreq/schedutil/down_rate_limit_us 200
 write /sys/devices/system/cpu/cpufreq/schedutil/iowait_boost_enable 1
-
-# SchedTune
-write /dev/stune/background/schedtune.boost 0
-write /dev/stune/foreground/schedtune.prefer_idle 1
-write /dev/stune/top-app/schedtune.prefer_idle 1
-write /dev/stune/top-app/schedtune.boost 0
-write /dev/stune/top-app/schedtune.sched_boost 1
 
 # Power Efficient Workqueue
 chmod 0644 /sys/module/workqueue/parameters/power_efficient
