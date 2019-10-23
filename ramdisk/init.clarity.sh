e7f797254fb5f3ef5ddf67b26214aa82b4fa24dc
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-10-23 10:59:19 +0700

    ramdisk: increase thermal limit to 55 C
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- ramdisk/init.clarity.sh
+++ ramdisk/init.clarity.sh
@@ -20,9 +20,9 @@ write /sys/module/msm_thermal/core_control/enabled 0
 chmod 0644 /sys/module/msm_thermal/vdd_restriction/enabled
 write /sys/module/msm_thermal/vdd_restriction/enabled 0
 write /sys/module/msm_thermal/parameters/core_limit_temp_degC 60
-write /sys/module/msm_thermal/parameters/limit_temp_degc 55
-write /sys/module/msm_thermal/parameters/temp_threshold 50
-write /sys/module/msm_thermal/parameters/poll_ms 500
+write /sys/module/msm_thermal/parameters/limit_temp_degc 60
+write /sys/module/msm_thermal/parameters/temp_threshold 55
+write /sys/module/msm_thermal/parameters/poll_ms 1000
 
 # GPU Values
 write /sys/class/kgsl/kgsl-3d0/devfreq/governor "msm-adreno-tz"
