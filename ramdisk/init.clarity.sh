a8b130ce93e550288f4d9f633c9a41a3a60cdd47
Dicky Herlambang <herlambangdicky5@gmail.com>
2020-01-02 09:55:19 +0700

    ramdisk: sync from latest kernel build
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- ramdisk/init.clarity.sh
+++ ramdisk/init.clarity.sh
@@ -13,16 +13,10 @@ for group in background foreground rt top-app audio-app; do
 write /dev/stune/top-app/schedtune.boost 0
 
 # Thermal Engine (Intelli Thermal v3)
-chmod 0644 /sys/module/msm_thermal/parameters/enabled
-write /sys/module/msm_thermal/parameters/enabled 1
 chmod 0644 /sys/module/msm_thermal/core_control/enabled
 write /sys/module/msm_thermal/core_control/enabled 0
 chmod 0644 /sys/module/msm_thermal/vdd_restriction/enabled
 write /sys/module/msm_thermal/vdd_restriction/enabled 0
-write /sys/module/msm_thermal/parameters/core_limit_temp_degC 60
-write /sys/module/msm_thermal/parameters/limit_temp_degc 60
-write /sys/module/msm_thermal/parameters/temp_threshold 55
-write /sys/module/msm_thermal/parameters/poll_ms 1000
 
 # GPU Values
 write /sys/class/kgsl/kgsl-3d0/devfreq/governor "msm-adreno-tz"
@@ -50,9 +44,3 @@ write /sys/block/mmcblk1/queue/read_ahead_kb 512
 write /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk 0
 write /sys/module/process_reclaim/parameters/enable_process_reclaim 0
 write /sys/module/lowmemorykiller/parameters/debug_level 0
-
-# Misc Audio Optimizations
-write /sys/module/snd_soc_wcd9330/parameters/high_perf_mode 0
-write /sys/module/snd_soc_wcd9335/parameters/huwifi_mode 0
-write /sys/module/snd_soc_wcd9335/parameters/low_distort_amp 0
-write /sys/module/snd_soc_wcd9xxx/parameters/impedance_detect_en 0
